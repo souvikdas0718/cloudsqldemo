b41011e199a3128f4eb43ba418af1014c8298c8d
souvikdas <Souvik.Das@dal.ca>
2021-06-03 12:04:10 -0300

    Adding codes

---
+++ database.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the CloudSQL instance...');
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const connection = require('./database');
+
+
+// simple GET api
+app.get('/home', function (req, res) {
+    res.send("Hello world!");
+});
+
+// GET api to print results of employee table
+app.get('/home/moderators', function (req, res) {
+    connection.query(
+        "USE lab4DB");
+    connection.query(
+        "SELECT * FROM `lab4DB`",
+        function (error, results, fields) {
+            if (error) throw error;
+            res.json(results);
+        }
+    );
+});
+
+app.listen(3000, function () {
+    console.log("Listening on port 3000");
+});
